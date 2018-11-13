ae4b0839c4bb56c7a626a565a32fc93d331ebd2e
philten <philten@cern.ch>
2018-11-13 20:47:31 +0000

    Compilation errors gone for section 2, need bibliography now.

---
+++ merge.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import shutil
+
+# Determine the repeated commands.
+tex = open("report.tex")
+tmp = open("tmp.tex", "w")
+cmds = []
+for line in tex:
+    if line.startswith(r"\newcommand{"):
+        cmd = line[11:].split("}")[0]
+        if cmd in cmds:
+            ob = line.count("{")
+            cb = line.count("}")
+            while ob != cb:
+                line = next(texd)
+                ob += line.count("{")
+                cb += line.count("}")
+        else:
+            cmds += [cmd]
+            tmp.write(line)
+    else: tmp.write(line)
+tex.close()
+tmp.close()
+shutil.move("tmp.tex", "report.tex")
