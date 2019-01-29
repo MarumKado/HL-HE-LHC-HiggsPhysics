6a0f2b8724d68fe5a7f00c72822c0315f66bdca9
philten <philten@cern.ch>
2019-01-29 11:45:03 +0000

    Fixed Leonidas institute.

--- authors.py
+++ authors.py
@@ -45,52 +45,56 @@ def institute(iid):
     return r"\iinstitute{%i}{%s}" % (iid, toascii(name))
 
 ###############################################################################
-# Load the CSV file.
-dat = urlopen("https://docs.google.com/spreadsheets/d/"
-              "167WsB2xCklZalMwqlDOZqb3BKUZTKQFi5jlz2YRt6ak/export?format=csv")
-reader = csv.reader(codecs.iterdecode(dat, "utf-8"), delimiter = ",")
-header = next(reader)
-authors = {}
-
-# Load the authors.
-for row in reader:
-    if row[6] == "n": continue
-    row = [val.strip() for val in row]
-    last, first, aid, iids, inames = row[0:5]
-    if not first or not last: continue
-    iids = iids.split(",")
-    inames = inames.split(";")
-    insts = [(iid.strip(), iname.strip()) for iid, iname in zip(iids, inames)]
-    key = " ".join([last, first])
-    if key in authors: authors[key][2] += insts
-    else: authors[key] = [aid, " ".join([first, last]), insts]
-dat.close()
-
-# Write the preamble.
-tex = open("authors.tex", "w")
-tex.write(r"\newcounter{instituteref}" + "\n")
-tex.write(r"\newcommand{\iinstitute}[2]{\refstepcounter{instituteref}\label{#1}"
-          r"$^{\ref{#1}}$\href{http://inspirehep.net/record/#1}{#2}}" + "\n")
-tex.write(r"\newcommand{\iauthor}[3]{"
-          r"\href{http://inspirehep.net/record/#1}{#2}$^{#3}$}")
-
-# Write the conveners.
-conveners = ["Cepeda M.", "Gori S.", "Ilten P.", "Kado M.", "Riva F."]
-institutes = collections.OrderedDict()
-tex.write("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
-          r"\author{Convenors: \\ " + "\n")
-tex.write(",\n".join([author(authors, institutes, name) for name in conveners]))
-for name in conveners: del authors[name]
-
-# Write the authors.
-tex.write("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
-          r"\\ \vspace*{4mm} Contributors: \\" + "\n")
-tex.write(",\n".join([author(authors, institutes, name) for name in
-                      sorted(authors, key = lambda key: key.lower())]))
-
-# Write the institutes.
-tex.write("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
-          r"\vspace*{1cm}} \institute{\small " + "\n")
-tex.write(";\n".join([institutes[iid] for iid in institutes]))
-tex.write(r"}" + "\n")
-tex.close()
+if __name__ == "__main__":
+    # Load the CSV file.
+    dat = urlopen("https://docs.google.com/spreadsheets/d/167WsB2xCklZalMwql"
+                  "DOZqb3BKUZTKQFi5jlz2YRt6ak/export?format=csv")
+    reader = csv.reader(codecs.iterdecode(dat, "utf-8"), delimiter = ",")
+    header = next(reader)
+    authors = {}
+    
+    # Load the authors.
+    for row in reader:
+        if row[6] == "n": continue
+        row = [val.strip() for val in row]
+        last, first, aid, iids, inames = row[0:5]
+        if not first or not last: continue
+        iids = iids.split(",")
+        inames = inames.split(";")
+        insts = [(iid.strip(), iname.strip()) for iid, iname in
+                 zip(iids, inames)]
+        key = " ".join([last, first])
+        if key in authors: authors[key][2] += insts
+        else: authors[key] = [aid, " ".join([first, last]), insts]
+    dat.close()
+    
+    # Write the preamble.
+    tex = open("authors.tex", "w")
+    tex.write(r"\newcounter{instituteref}" + "\n")
+    tex.write(r"\newcommand{\iinstitute}[2]{\refstepcounter{instituteref}"
+              r"\label{#1}$^{\ref{#1}}$\href{http://inspirehep.net/record/#1}"
+              r"{#2}}" + "\n")
+    tex.write(r"\newcommand{\iauthor}[3]{"
+              r"\href{http://inspirehep.net/record/#1}{#2}$^{#3}$}")
+    
+    # Write the conveners.
+    conveners = ["Cepeda M.", "Gori S.", "Ilten P.", "Kado M.", "Riva F."]
+    institutes = collections.OrderedDict()
+    tex.write("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
+              r"\author{Convenors: \\ " + "\n")
+    tex.write(",\n".join([author(authors, institutes, name)
+                          for name in conveners]))
+    for name in conveners: del authors[name]
+    
+    # Write the authors.
+    tex.write("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
+              r"\\ \vspace*{4mm} Contributors: \\" + "\n")
+    tex.write(",\n".join([author(authors, institutes, name) for name in
+                          sorted(authors, key = lambda key: key.lower())]))
+    
+    # Write the institutes.
+    tex.write("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
+              r"\vspace*{1cm}} \institute{\small " + "\n")
+    tex.write(";\n".join([institutes[iid] for iid in institutes]))
+    tex.write(r"}" + "\n")
+    tex.close()
